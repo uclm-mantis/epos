668ad4bb6bb0deec1c7c459ac4c2d21aefaad2b9
Francisco Moya <francisco.moya@uclm.es>
2026-03-20 08:37:42 +0100

    more changes

--- src/motor_console.h
+++ src/motor_console.h
@@ -8,7 +8,7 @@ extern "C" {
 #endif
 
 /**
- * @brief Registra los comandos relacionados con el control del motor en la consola interactiva.
+ * @brief Registra los comandos para el control de motores CiA 402 en la consola interactiva.
  */
 void motor_register_commands(void);
 
