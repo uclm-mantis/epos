2824f0e4d204869ccdcbf88ec1e44a1fd62e3277
Francisco Moya <francisco.moya@uclm.es>
2026-05-20 20:22:35 +0200

    fixed compilation issue

--- src/canopen_console.c
+++ src/canopen_console.c
@@ -8,10 +8,7 @@
 #include "sdkconfig.h"
 #include "driver/uart.h"
 #include "driver/uart_vfs.h"
-#ifndef CONFIG_USJ_ENABLE_USB_SERIAL_JTAG
-#define CONFIG_USJ_ENABLE_USB_SERIAL_JTAG 0
-#endif
-#if CONFIG_USJ_ENABLE_USB_SERIAL_JTAG
+#if CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG_ENABLED
 #include "driver/usb_serial_jtag.h"
 #include "driver/usb_serial_jtag_vfs.h"
 #endif
@@ -669,7 +666,7 @@ void tcp_console_task(void *arg);
 
 static void configure_usb_serial_jtag_console(const canopen_console_cfg_t *cfg)
 {
-#if CONFIG_USJ_ENABLE_USB_SERIAL_JTAG
+#if CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG_ENABLED
     fflush(stdout);
     fsync(fileno(stdout));
 
@@ -689,7 +686,7 @@ static void configure_usb_serial_jtag_console(const canopen_console_cfg_t *cfg)
     setvbuf(stdin, NULL, _IONBF, 0);
 #else
     (void)cfg;
-    ESP_LOGE(TAG, "USB-JTAG console requires CONFIG_USJ_ENABLE_USB_SERIAL_JTAG=y");
+    ESP_LOGE(TAG, "USB-JTAG console requires CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG_ENABLED=y");
     ESP_ERROR_CHECK(ESP_ERR_NOT_SUPPORTED);
 #endif
 }
